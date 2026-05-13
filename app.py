b6389c916b1c97163a5fe73e0e19d6727924842d
FreysiY <freysi345@gmail.com>
2026-05-12 20:06:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,23 @@ st.sidebar.title("Parámetros")
 
 st.write("Elaborado por: Freysi Zurita")
 
+st.sidebar.image("DMC.png") #sidebar se coloca para que se muestre no en el centro sino hacia un lado
+
 sesion = st.sidebar.selectbox("Seleccione una sesión", ["Sesión 1","Sesión 2","Sesión 3","Sesión 4"] )
 
 if sesion == "Sesión 1":
   st.write("Bienvenido a la sesión 1")
+  st.image("Python_logo.png")
 
 elif sesion == "Sesión 2":
-  st.write("Bienvenido a la sesión 2")
+  st.write("Bienvenido a la sesión 2") #st.write equivale a print
+  
+  precio = st.number_input("Ingrese el precio del producto")
+  descuento = st.number_input("Ingrese el descuento del producto")
+
+  precio_final_producto = precio - (precio*descuento)
+
+  st.write("El precio final del producto es: ",precio_final_producto)
 
 elif sesion == "Sesión 3":
   st.write("Bienvenido a la sesión 3")
